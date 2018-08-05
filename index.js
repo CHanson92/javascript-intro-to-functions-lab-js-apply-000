09653249cf23e68028343b456738bf02ffc3ab26
Chris Hanson <blondyhanson@gmail.com>
2018-08-05 13:24:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ describe('logShout(string)', function() {
   })
 })
 
-
+function logShout(string)
+console.log(string.toUpperCase)
 
 describe('logWhisper(string)'), function() {
   it('calls console.log() its one argument in all lowercase', function() {
