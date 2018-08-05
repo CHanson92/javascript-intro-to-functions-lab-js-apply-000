ae7c6bdad26b7a97b26c7bbd0a02534ec06ca92d
Chris Hanson <blondyhanson@gmail.com>
2018-08-05 13:48:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,6 +67,12 @@ describe('sayHiToGrandma(string)'), function() {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
-    
+    return "I can't hear you!"
+  }
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
   }
 }
